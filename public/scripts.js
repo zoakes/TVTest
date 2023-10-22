135409581cb4357e4762f727db9080137ce31ba6
Zach <zach_oakes@outlook.com>
2023-10-22 16:55:02 -0500

    rn

--- public/scripts.js
+++ public/scripts.js
@@ -1,37 +1,5 @@
 // chartProperties, and chart are defined in index.js! DOTN redefine.
-
-// const zc = LightweightCharts.createChart(document.getElementById('zochart'), chartProperties);
-// const zc_candle = zc.addCandlestickSeries();
-
-// // Function to generate random candlestick data:
-// function generateCandle() {
-//     const time = Math.floor(Date.now() / 1000);
-//     const open = Math.random() * (100 - 85) + 85;
-//     const close = open + Math.random() * 10 - 5;
-//     const high = Math.max(open, close) + Math.random() * 5;
-//     const low = Math.min(open, close) - Math.random() * 5;
-
-//     return {
-//         time: time,
-//         open: open,
-//         high: high,
-//         low: low,
-//         close: close,
-//     };
-// }
-
-// // Update the chart with random data every 5 seconds:
-// setInterval(() => {
-//     const candle = generateCandle();
-//     candleSeries.update(candle);
-//     log(`Added candle at time ${candle.time}`);
-// }, 500);
-
-
-// ------------------------------ UPDATES 
-
-
-const zc_line = chart.addBaselineSeries();
+const lineSeries = chart.addBaselineSeries();
 
 // Function to generate a random data point
 function generateDataPoint(previousValue) {
@@ -71,7 +39,7 @@ const allMarkers = [];
 setInterval(() => {
     lastTime = lastTime + 5; // Increment time by 5 seconds
     lastValue = generateDataPoint(lastValue);
-    zc_line.update({
+    lineSeries.update({
         time: lastTime,
         value: lastValue,
     });
@@ -79,7 +47,7 @@ setInterval(() => {
     const marker = generateMarker(lastTime);
     if (marker) {
         allMarkers.push(marker);
-        zc_line.setMarkers(allMarkers); // Set all markers, including the new one
+        lineSeries.setMarkers(allMarkers); // Set all markers, including the new one
     }
 
     // Adjust the visible range of the time scale:
