82f30e82644c63652f9fc0e68be6368ce53e8f71
Zach <zach_oakes@outlook.com>
2023-10-22 14:50:28 -0500

    closer?

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const chart = LightweightCharts.createChart(document.getElementById('chart-container'), {
-        width: document.documentElement.clientWidth * 0.8,
-        height: 300
-    });
 
-    const lineSeries = chart.addLineSeries();
-    lineSeries.setData([
-        { time: '2019-04-11', value: 80.01 },
-        { time: '2019-04-12', value: 96.63 },
-        // ... add more data points as needed
-    ]);
-});
+
+import { createChart } from 'lightweight-charts';
+
+console.log("scripts.js is loaded");
+
+
+function addMarkerToChart(data) {
+    lineSeries.update(data);
+    
+    if (data.action) {
+        const marker = {
+            time: data.time,
+            position: data.action === 'buy' ? 'belowBar' : 'aboveBar',
+            color: data.action === 'buy' ? 'green' : 'red',
+            shape: data.action === 'buy' ? 'arrowUp' : 'arrowDown',
+        };
+        lineSeries.setMarkers([marker]);
+    }
+}
+
+
+function generateRandomData(previousValue) {
+    const change = (Math.random() - 0.5) * 2;
+    const newValue = previousValue + change;
+    const currentDate = new Date();
+    return {
+        time: `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`,
+        value: newValue
+    };
+}
+
+
+const chart = createChart(document.getElementById('chart'), { width: 400, height: 300 });
+const lineSeries = chart.addLineSeries();
+
+
+let lastValue = 100;  // Starting value, can be any initial value you prefer
+
+// Set initial data
+const initialData = [
+    { time: '2023-10-21', value: 98.5 },
+    { time: '2023-10-22', value: 99.5 },
+    { time: '2023-10-23', value: lastValue }
+];
+lineSeries.setData(initialData);
+
+// Real-time data updates (every 5s)
+setInterval(() => {
+    const newData = generateRandomData(lastValue);
+    lineSeries.update(newData);
+    lastValue = newData.value;
+}, 5000);
