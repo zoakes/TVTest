9c28ad0ae344d3c22036b3d63ab10dc728798e52
Zach <zach_oakes@outlook.com>
2023-10-22 16:52:31 -0500

    works w incognito browser, and live_Server enables hot reload

--- public/scripts.js
+++ public/scripts.js
@@ -96,3 +96,7 @@ setInterval(() => {
 
 // chart.timeScale().fitContent(); // this is supposed to auto-scale, not working right?
 log("fuck you javascript.");
+
+
+// WORKS with live_server extension
+// AND incognito window!
